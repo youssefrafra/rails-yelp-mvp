3a323cefc847704f7751932a230fcf96fb682a8e
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-06-09 15:06:17 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times{
+    restaurant = Restaurant.new(name: Faker::Restaurant.name   , address: Faker::Address.street_address , phone_number: Faker::PhoneNumber.cell_phone, category: category.sample)
+    restaurant.save!
+}
